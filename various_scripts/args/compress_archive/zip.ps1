4009b6b69184e73fc61666c5c950752b03a98109
cywisp <cywisp@gmail.com>
2020-05-22 14:49:04 -0400

    updated

--- various_scripts/args/compress_archive/zip.ps1
+++ various_scripts/args/compress_archive/zip.ps1
@@ -1,8 +1,32 @@
-# Creates a zip file
+# Will create a zip archive given archive name and target folder
 
+### Parameters ###
 param(
-	[string]$archive_name
-	[string]$target_archive
+	[string]$archive_name,
+	[string]$target
 )
 
-write-host($($args.len()))
+### Functions ###
+function usage {
+	Write-Host("[!] Usage: ./$($MyInvocation.MyCommand.Name) <Archive Name> <Target Directory>")
+	exit
+}
+
+function zip_archive {
+	Write-Host("[+] Compressing $($target)...")
+	Compress-Archive -Path $target -DestinationPath $archive_name -CompressionLevel "Fastest"	
+	Write-Host("[*] Done...")
+}
+
+### Main ###
+if ($PSBoundParameters.Count -ne 2) {
+	usage
+	Write-Host("[x] Please supply at least two parameters... ")
+} elseif ((Test-Path $target) -eq $False ){
+	Write-Host("[x] $($target) does not exist...")
+	usage
+} else {
+	zip_archive
+}
+
+
